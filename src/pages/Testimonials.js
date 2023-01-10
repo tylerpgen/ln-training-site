c9de96c413d38f34ac6a6a0963e71f8c95d12850
Tyler Gener <tylergen33@gmail.com>
2023-01-10 11:46:28 -0800

    Updated testimonials page for client with new entries that were requested

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -7,6 +7,12 @@ import ShevBef from "../assets/new-pics/shevBefore.png";
 import ShevAft from "../assets/new-pics/shevAfter.png";
 import AshB from "../assets/new-pics/ashkanBefore.png";
 import AshA from "../assets/new-pics/ashkanAfter.png";
+import Caro from "../assets/new-pics/caro1.png";
+import CaroB from "../assets/new-pics/caro2.png";
+import JasonB from "../assets/new-pics/JasonB.png";
+import JasonA from "../assets/new-pics/JasonA.png";
+import RickyB from "../assets/new-pics/RickyB.png";
+import RickyA from "../assets/new-pics/RickyA.png";
 
 function Testimonials() {
   return (
@@ -42,6 +48,30 @@ I started training with him about 2 years ago, and haven’t looked back since. 
                     in my body and overall health goals. I highly recommend anyone interested in any form of training to work with Lukas.'"
             img={AshB}
             img2={AshA}
+          />
+        </div>
+        <div className="card3">
+          <Card
+            name="Caroline Q."
+            test="'10/10 cool dude makes working out fun, he yells when you're BS'ing, and makes you do better, so again, 10/10 recommend'"
+            img={Caro}
+            img2={CaroB}
+          />
+        </div>
+        <div className="card3">
+          <Card
+            name="Jason M."
+            test="'First and foremost, Lukas is the best personal trainer I've ever had in my life. Since a young age, I struggled with being overweight and eating a bad diet. I was fortunate enough to meet my savior, Lukas, who helped me not only lose a considerable amount of weight, but also enhance my lifestyle choices and nutrition. I've attempted to lose weight before, however, I could never stay consistent and it wouldn't go well. Lucky for me, Lukas helped me with all of that. I finally saw great results in weight and not to mention my strength and muscle gains. In addition, I also have scoliosis and since I began LN Training, my pain got better and Lukas was great at working around it. 10/10 I would recommend Lukas to anyone!'"
+            img={JasonB}
+            img2={JasonA}
+          />
+        </div>
+        <div className="card3">
+          <Card
+            name="Ricky M."
+            test="'Working remotely with LN Training has provided me with a system and routine that works very well for me. He has provided me with exercises that work towards achieving my fitness goals and also worked with me to account for an injury that occurred a couple months before working with Lukas. In the first 2 months I have lost almost 8 pounds while also gaining noticeable muscle improvements! I'm feeling more confident than ever before.'"
+            img={RickyB}
+            img2={RickyA}
           />
         </div>
       </div>
