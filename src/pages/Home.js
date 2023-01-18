50c210998cb9a2e0df460d3e3389b030fabcde50
Tyler Gener <tylergen33@gmail.com>
2023-01-18 00:59:37 -0800

    Changed landing page quote per client request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/Home.css';
-import Portrait from '../assets/new-pics/newportrait.png'
-import AboutMe from '../components/AboutMe';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
+import Portrait from "../assets/new-pics/newportrait.png";
+import AboutMe from "../components/AboutMe";
 
 function Home() {
-    return (
-        <div className="home">
-            <div className="cta">
-                <h1>"Health and Fitness are lifestyle choices, and you get a chance to achieve them everyday"</h1 >
-                <p>Sign up TODAY for 50% off your first session!</p>
-                <span className='home-button'>
-                    <Link to="/contact">
-                        <button id="signUp"> SIGN UP </button>
-                    </Link>
-                </span>
-            </div>
-            <div className="portrait fadeIn">
-                <img src={Portrait} alt="lukas" className="portrait" />
-            </div>
-            <div className='about-me'>
-                <h1 className='about-me-header'>About Me</h1>
-                <AboutMe />
-            </div>
-        </div>
-    )
+  return (
+    <div className="home">
+      <div className="cta">
+        <h1>"Health, performance, and longevity are not a matter of chance, it's a matter of choice"</h1>
+        <p>Sign up TODAY for 50% off your first session!</p>
+        <span className="home-button">
+          <Link to="/contact">
+            <button id="signUp"> SIGN UP </button>
+          </Link>
+        </span>
+      </div>
+      <div className="portrait fadeIn">
+        <img src={Portrait} alt="lukas" className="portrait" />
+      </div>
+      <div className="about-me">
+        <h1 className="about-me-header">About Me</h1>
+        <AboutMe />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
-
-
