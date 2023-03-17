7b3c2abe93f52121bf590a3df5d71a9592b66306
Tyler Gener <tylergen33@gmail.com>
2023-03-17 03:02:49 -0700

    Created new interactive Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import NewNavbar from "./components/NewNavbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Testimonials from "./pages/Testimonials";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
+
+const theme = createTheme();
+
 function App() {
   return (
     <Router>
-      <div className="content-wrapper">
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/testimonial" exact element={<Testimonials />} />
-          <Route path="/contact" exact element={<Contact />} />
-        </Routes>
-        <Footer className="footer" />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="content-wrapper">
+          <NewNavbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/testimonial" exact element={<Testimonials />} />
+            <Route path="/contact" exact element={<Contact />} />
+          </Routes>
+          <Footer className="footer" />
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ src/components/NewNavbar.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { AppBar, IconButton, Link, SwipeableDrawer, Toolbar, Typography, Divider, List, ListItem } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+const navigationLinks = [
+  { name: "Home", href: "/" },
+  { name: "Testimonials", href: "/testimonial" },
+  { name: "Contact", href: "/contact" },
+  { name: "Products", href: "/" },
+];
+
+const NewNavbar = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky" sx={{ backgroundColor: "black" }}>
+      <Toolbar sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
+        {isNonMobileScreens
+          ? navigationLinks.map((item) => (
+              <Link
+                color="textPrimary"
+                variant="button"
+                underline="none"
+                href={item.href}
+                sx={{ mx: 10, color: "white", "&:hover": { color: "white" } }}
+              >
+                <Typography variant="h6" fontWeight="bold">
+                  {item.name}
+                </Typography>
+              </Link>
+            ))
+          : null}
+        {!isNonMobileScreens ? (
+          <IconButton sx={{ ml: "auto" }}>
+            <MenuIcon sx={{ color: "white", fontSize: 80 }} onClick={() => setOpen(true)} />
+          </IconButton>
+        ) : null}
+      </Toolbar>
+      <SwipeableDrawer anchor="right" open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
+        <IconButton>
+          <ChevronRightIcon sx={{ color: "black", fontSize: 80 }} onClick={() => setOpen(false)} />
+        </IconButton>
+        <Divider sx={{ backgroundColor: "black", height: "4px" }} />
+        <List>
+          {navigationLinks.map((item) => (
+            <ListItem>
+              <Link
+                color="textPrimary"
+                variant="button"
+                underline="none"
+                href={item.href}
+                sx={{ color: "black", "&:hover": { color: "black" } }}
+              >
+                <Typography variant="h6" fontWeight="bold" sx={{ fontSize: "3rem" }}>
+                  {item.name}
+                </Typography>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+      </SwipeableDrawer>
+    </AppBar>
+  );
+};
+
+export default NewNavbar;
