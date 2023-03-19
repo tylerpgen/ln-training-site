aaf77ec22a407c89c8548091826368aa2340491e
Tyler Gener <tylergen33@gmail.com>
2023-03-19 00:23:31 -0700

    Redesigned home page

--- src/pages/NewHome.jsx
+++ src/pages/NewHome.jsx
@@ -1,9 +1,10 @@
 import { useTheme } from "@emotion/react";
-import { Typography, Button, Container, Link, Box, Grow, Fade, useScrollTrigger } from "@mui/material";
+import { Typography, Button, Link, Box, Grow, Fade, useScrollTrigger, Paper } from "@mui/material";
 import { styled } from "@mui/system";
 import { useState } from "react";
 import GymPic from "../assets/new-pics/gympic.png";
 import Portrait from "../assets/new-pics/newportrait.png";
+import TrainingPic1 from "../assets/new-pics/trainingpic1.png";
 
 const NewHome = () => {
   const [hasFadeIn, setHasFadeIn] = useState(false);
@@ -13,8 +14,8 @@ const NewHome = () => {
     backgroundColor: "white",
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
     [theme.breakpoints.up("lg")]: {
-      justifyContent: "center",
       alignItems: "center",
     },
     minHeight: "100vh",
@@ -55,6 +56,18 @@ const NewHome = () => {
     },
   }));
 
+  const TrainingPic = styled("img")(({ theme }) => ({
+    alignItems: "center",
+    width: "100%",
+    padding: "15px",
+    margin: "15px auto",
+    borderRadius: "25px",
+    [theme.breakpoints.up("lg")]: {
+      width: "50%",
+      padding: "0",
+    },
+  }));
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0.5,
@@ -67,6 +80,7 @@ const NewHome = () => {
   };
 
   return (
+    // Landing Page
     <>
       <Box sx={{ display: "flex", flexDirection: "column", minWidth: "100%" }}>
         <BackgroundImage>
@@ -76,7 +90,8 @@ const NewHome = () => {
               fontWeight="bold"
               color="white"
               sx={{
-                mb: "10px",
+                mb: "8px",
+
                 [theme.breakpoints.up("lg")]: {
                   fontSize: "5rem",
                 },
@@ -87,6 +102,7 @@ const NewHome = () => {
             <Typography
               variant="h6"
               fontWeight="light"
+              fontStyle="italic"
               color="white"
               sx={{
                 [theme.breakpoints.up("lg")]: {
@@ -94,9 +110,9 @@ const NewHome = () => {
                 },
               }}
             >
-              Your Fitness Your Way Your Time
+              Personal Training | Sports Performance | Online Coaching & Programming
             </Typography>
-            <Container sx={{ mt: "10px" }}>
+            <Box sx={{ mt: "10px" }}>
               <Link href="/contact" sx={{ textDecoration: "none" }}>
                 <Button
                   variant="contained"
@@ -116,10 +132,13 @@ const NewHome = () => {
                   Sign UP Now
                 </Button>
               </Link>
-            </Container>
+            </Box>
           </QuoteContainer>
         </BackgroundImage>
       </Box>
+
+      {/* Portrait Section with Name, Description, and Button call to action. */}
+
       <Box>
         <StyledBox>
           {hasFadeIn ? (
@@ -191,7 +210,7 @@ const NewHome = () => {
           ) : (
             <>
               <Fade in={trigger} timeout={1500} onEntered={handleFadeIn}>
-                <PortraitPic src={Portrait} alt="lukas" className="portrait" />
+                <PortraitPic src={Portrait} alt="lukas" />
               </Fade>
               <Grow in={trigger} timeout={900} onEntered={handleFadeIn}>
                 <Typography
@@ -265,6 +284,60 @@ const NewHome = () => {
               </Grow>
             </>
           )}
+        </StyledBox>
+      </Box>
+      {/* About Me Section */}
+      <Box>
+        <StyledBox
+          sx={{
+            backgroundColor: "#2A2A2A",
+          }}
+        >
+          <TrainingPic src={TrainingPic1} />
+          <Box
+            sx={{
+              width: "100%",
+              px: 1,
+              mb: "25px",
+              [theme.breakpoints.up("lg")]: {
+                maxWidth: "700px",
+              },
+              [theme.breakpoints.only("xl")]: {
+                maxWidth: "1050px",
+              },
+            }}
+          >
+            <Paper elevation={5} sx={{ p: 2, margin: "0 8px" }}>
+              <Typography variant="h5" fontWeight="bold" sx={{ textAlign: "center" }}>
+                About Me
+              </Typography>
+              <Typography variant="body1" sx={{ mt: "8px", textIndent: "1.5em" }}>
+                Hello! My name is Lukas Nouri, and my love for fitness and sports began at a young age. Throughout my
+                years of competition, I have gained extensive knowledge and experience in Basketball, Baseball,
+                Football, Soccer, Brazilian jiu-jitsu, Muay Thai, and Swimming. From the many tough years of training
+                and competitive athletics, I also experienced injuries that required surgery and rehabilitation for both
+                my lower back and hip. My passion for sports and experience with acute injuries, led me to learn more
+                about fitness, and my passion for coaching made me want to share my knowledge with others.
+              </Typography>
+              <Typography variant="body1" sx={{ mt: "15px", textIndent: "1.5em" }}>
+                I studied kinesiology at Cal State University, Northridge and graduated with my Bachelors in Exercise
+                Science. Throughout my years of study I worked as an intern for many places with a wide variety of
+                kinesiology practices. Some of these include: Athletic trainer for Football and Volleyball at Los
+                Angeles Pierce College and Physical therapy at Northridge Hospital. I quickly developed the ability to
+                train athletes, untrained individuals, and all populations of people returning from injury appropriately
+                and effectively. After my studies, I earned my CSCS certification and decided to build and run my own
+                private personal training business; LN Training.
+              </Typography>
+              <Typography variant="body1" sx={{ mt: "15px", textIndent: "1.5em" }}>
+                Since I've started my independent practice I have experienced training a wide variety of clientele which
+                includes everything from coaching my 8 year old niece in basketball fundamentals to post surgical
+                rehabilitation with senior population, those making the transition from sedentary to active lifestyles,
+                and even training Division-1 athletes. With my knowledge and combined experiences I have created a
+                diverse foundation for coaching and helping others achieve their health, nutrition, sports performance
+                and fitness goals.
+              </Typography>
+            </Paper>
+          </Box>
         </StyledBox>
       </Box>
     </>
