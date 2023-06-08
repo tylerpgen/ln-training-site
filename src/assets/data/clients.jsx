ef3530b3a8d4afb5e7cc935dac30d6761f536f93
Tyler Gener <tylergen33@gmail.com>
2023-06-08 12:05:25 -0700

    updated testimonial page to client's request

--- src/assets/data/clients.jsx
+++ src/assets/data/clients.jsx
@@ -1,7 +1,7 @@
 import AshkanB from "../new-pics/ashkanB.png";
 import AshkanA from "../new-pics/ashkanAfter.png";
-import ShevBef from "../new-pics/shevBefore.png";
-import ShevAft from "../new-pics/shevAfter.png";
+import ShevBef from "../new-pics/shavivbefore.jpg";
+import ShevAft from "../new-pics/shavivafter.jpg";
 import Caro from "../new-pics/caronewafter.jpg";
 import CaroB from "../new-pics/caronewbefore.jpg";
 import JasonB from "../new-pics/JasonB.png";
@@ -21,6 +21,19 @@ export const clientData = [
     review: `"Lukas has been the perfect trainer for my postpartum fitness journey. He pushes me when needed but at the same time understands my limitations. I highly recommend Lukas for anyone looking to start working out or enhance their current fitness regiment."`,
   },
   {
+    beforePic: CaroB,
+    afterPic: Caro,
+    name: "Caroline Q.",
+    review: `"For as long as I can remember, I’ve struggled with my physical health and mental health.  I was so nervous to start personal training to help combat these issues, but when I met Lukas on our first session, I knew he was the right fit for me! He is knowledgeable in his craft, but more importantly personable and patient with his clients and their issues going on outside of the gym. Fast forward 1 year later of consistently exercising with him, I’ve gone for 155lbs to 125lbs and I’ve never felt so confident in my self-image. I’m stronger and more confident than ever. Lukas has helped me so much with finding a healthier lifestyle and I can honestly say since starting with him, I am better version of myself down to the soul. If it weren’t for him, I don’t know where I’d be today. He is not only an amazing coach, but an amazing friend. 10/10 recommend to anyone starting a fitness journey."`,
+  },
+  {
+    beforePic: ShevBef,
+    afterPic: ShevAft,
+    name: "Shaviv D.",
+    review: `"Lukas is extremely knowledgeable of anatomy and his craft! He knows exactly how to ease you into a program and a routine, to not get you discouraged and to bring you the best results possible.
+    I started training with him about 2 years ago, and haven’t looked back since. He makes programs and sessions that put you to work, but also is sure to incorporate exercises that you enjoy. You always leave feeling good!"`,
+  },
+  {
     beforePic: MarcusB,
     afterPic: MarcusA,
     name: "Marcus B.",
@@ -28,12 +41,6 @@ export const clientData = [
     Since this ain't my first rodeo, I can honestly say that what separates Lukas from other personal trainers is the "personal" part. 
     I've never had another trainer track progress, isolate trouble areas or create a program that felt so individual to my needs. 
     At 43, I'm literally in the best shape that I've ever been. Thanks!"`,
-  },
-  {
-    beforePic: CaroB,
-    afterPic: Caro,
-    name: "Caroline Q.",
-    review: `"For as long as I can remember, I’ve struggled with my physical health and mental health.  I was so nervous to start personal training to help combat these issues, but when I met Lukas on our first session, I knew he was the right fit for me! He is knowledgeable in his craft, but more importantly personable and patient with his clients and their issues going on outside of the gym. Fast forward 1 year later of consistently exercising with him, I’ve gone for 155lbs to 125lbs and I’ve never felt so confident in my self-image. I’m stronger and more confident than ever. Lukas has helped me so much with finding a healthier lifestyle and I can honestly say since starting with him, I am better version of myself down to the soul. If it weren’t for him, I don’t know where I’d be today. He is not only an amazing coach, but an amazing friend. 10/10 recommend to anyone starting a fitness journey."`,
   },
   {
     beforePic: AshkanB,
@@ -46,13 +53,6 @@ export const clientData = [
     afterPic: JasonA,
     name: "Jason M.",
     review: `"First and foremost, Lukas is the best personal trainer I've ever had in my life. Since a young age, I struggled with being overweight and eating a bad diet. I was fortunate enough to meet my savior, Lukas, who helped me not only lose a considerable amount of weight, but also enhance my lifestyle choices and nutrition. I've attempted to lose weight before, however, I could never stay consistent and it wouldn't go well. Lucky for me, Lukas helped me with all of that. I finally saw great results in weight and not to mention my strength and muscle gains. In addition, I also have scoliosis and since I began LN Training, my pain got better and Lukas was great at working around it. 10/10 I would recommend Lukas to anyone!"`,
-  },
-  {
-    beforePic: ShevBef,
-    afterPic: ShevAft,
-    name: "Shaviv D.",
-    review: `"Lukas is extremely knowledgeable of anatomy and his craft! He knows exactly how to ease you into a program and a routine, to not get you discouraged and to bring you the best results possible.
-    I started training with him about 2 years ago, and haven’t looked back since. He makes programs and sessions that put you to work, but also is sure to incorporate exercises that you enjoy. You always leave feeling good!"`,
   },
   {
     beforePic: RickyB,
